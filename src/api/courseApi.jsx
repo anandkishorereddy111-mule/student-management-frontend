2c24311c197a0fb73de99c6765d3fba7dff2fe86
Anand Kishore Reddy <anandkishorereddy111@gmail.com>
2026-07-14 17:24:00 +0530

    Use env variable for API base URL

--- src/api/courseApi.jsx
+++ src/api/courseApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/courses";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getCourses = () => axios.get(BASE_URL);
 export const addCourse = (course) => axios.post(BASE_URL, course);
